23c7fc6b032ec85f497ec40d2f16fed2c6e6c2a8
A K M Zahiduzzaman <zhd.zmn@gmail.com>
2014-04-07 18:59:29 +0600

    removed stuff

--- db/migrate/20140407093243_remove_column_from_fixture.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumnFromFixture < ActiveRecord::Migration
-  def up
-    remove_column :fixtures, :group_id
-  end
-
-  def down
-    add_column :fixtures, :group_id, :integer
-  end
-end
